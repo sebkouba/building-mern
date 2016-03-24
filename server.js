fef17a0d48a98a7ecbc8996c1a5574697ae732fd
sebkouba <little.sebby@gmail.com>
2016-03-24 11:28:31 +0100

    add comments

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+// when a get request ist sent to '/' return "Hello World"
 app.get('/', (req, res) => {
   res.send("Hello World");
 });
 
+// start the server on port 3000
 var server = app.listen(3000, () => {
   console.log("Listening on Port " + server.address().port);
 });
